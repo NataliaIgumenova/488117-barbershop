224f6d33768dd64995aed6fba31c2c6ba88b1902
Your Name <natalia_15.16@mail.ru>
2017-08-11 18:40:55 +0300

    Добавлена разметка и файлы каталог и карточка товара

---
+++ barbershop-item.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <title>Барбершоп каталог</title>
+  </head>
+  <body>
+    <header class="header-page">
+        <nav class="header-nav">
+            <div class="header-top">
+                <a class="header-logo" href="index.html">
+                    <img src="http://placehold.it/150x100" alt="logo" width="111" height="24">
+                </a>
+                <ul class="header-nav">
+                    <li><a href="barbershop-info.html">Информация</a></li>
+                    <li><a href="barbershop-news.html">Новости</a></li>
+                    <li><a href="barbershop-price.html">Прайс лист</a></li>
+                    <li class="navigation-nav"><a href="barbershop-shop.html">Магазин</a></li>
+                    <li><a href="barbershop-contact.html">Контакты</a></li>
+                </ul>
+                <ul class="header-login">
+                    <li><a href="barbershop-login.html">Вход</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <main class="main-content">
+    
+        <h1 class="page-title">Набор для путешествий «Baxter of California»</h1>
+        <ul class="breadcrumbs">
+            <li><a href="index.html">Главная</a></li>
+            <li><a href="barbershop-shop.html">Магазин</a></li>
+            <li><a  href="#">Средства для ухода</a></li>
+            <li class="breadcrumbs-current">Набор для путешествий «Baxter of California»</li>
+        </ul>
+
+        <section class="product-photos">
+            <h2 class="invisible-headline">Изображения товара</h2>
+            <p class="product-photo-full">
+                <img src="img/product-big.jpg" width="460" height="498" alt="Фото всего набора">
+            </p>
+            <ul class="product-photo-previe">
+                <li><img src="img/product-small-1.jpg" width="140" height="149" alt="Фото в анфас"></li>
+                <li><img src="img/product-small-2.jpg" width="140" height="149" alt="Фото в профиль"></li>
+                <li><img src="img/product-small-3.jpg" width="140" height="149" alt="Фото отдельной части"></li>
+            </ul>
+        </section>
+
+        <section class="product-info">
+            <h2 class="invisible-headline">Описание товара</h2>
+            <p class="product-availability">Есть в наличии</p>
+            <p class="product-article">Артикул: dexter-ae</p>
+            <p class="product-text">Travel Kit – необходимый аксессуар во время любого путешествия. В аккуратной кожаной сумке находится все, что нужно для бритья и ухода за кожей во время рабочей поездки или отдыха: средство для умывания, увлажняющий крем, крем для бритья, крем после бритья, шампунь. Набор также может стать отличным подарком.</p>
+            <p class="product-price">
+                <b>2 900 &#8381;</b>
+                <a class="button" href="#">Купить</a>
+            </p>
+            <h3>В набор входят:</h3>
+            <ul>
+            <li>Средство для умывания (50 мл)</li>
+            <li>Увлажняющий крем (50 мл)</li>
+            <li>Крем для бритья (50 мл)</li>
+            <li>Крем после бритья, шампунь (50 мл)</li>
+            <li>Удобная кожаная косметичка</li>
+            </ul>
+        </section>
+        
+    </main>
+    
+    <footer>
+        <p>
+            Барбершоп "Бородинский"<br>
+            Адрес: г.Санкт-Петербург, ул. Б. Конюшенная, д. 9/18 <br>
+            <a href="map.html">Как найти нас?</a><br>
+            Телефон: +7(495)666-02-66
+        </p>
+        <div class="footer-social">
+            <b>Давайте жружить</b>
+            <ul>
+                <li><a href="social-battom" href="#">Вконтакте</a></li>
+                <li><a href="social-battom" href="#">Фэйсбук</a></li>
+                <li><a href="social-battom" href="#">Инстаграм</a></li>
+            </ul>
+        </div>
+        <p class="footer-copiright">
+            <b>Разработано:</b><a class="batton" href="https://echo.htmlacademia.ru">HTML Academi</a>
+        </p>
+    </footer>
+  </body>
+</html>
